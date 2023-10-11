82d17ac8196b951a4bc3326e641febf4ca2c36e6
olihm <106966316+olihm@users.noreply.github.com>
2023-10-11 08:56:43 -0300

    Add files via upload

---
+++ isis123/main.js
@@ -0,0 +1,19 @@
+function TocaSomBrasil(){
+    document.querySelector('#som_tecla_brasil').play();
+}
+document.querySelector('.tecla_brasil').onclick=TocaSomBrasil;
+
+function TocaSomMario(){
+    document.querySelector('#som_tecla_mario').play();
+}
+document.querySelector('.tecla_mario').onclick=TocaSomMario;
+
+function TocaSomPlantao(){
+    document.querySelector('#som_tecla_plantao').play();
+}
+document.querySelector('.tecla_plantao').onclick=TocaSomPlantao;
+
+function TocaSomShow (){
+    document.querySelector('#som_tecla_show').play();
+}
+document.querySelector('.tecla_show').onclick=TocaSomShow;
